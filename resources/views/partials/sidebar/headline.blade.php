69f25461d94ca8585b5bf04e07a3d5a765bc916a
Uriah Galang <g0ld3lux@gmail.com>
2017-06-21 18:40:50 +0800

    added APP ICON ENV and config

--- resources/views/partials/sidebar/headline.blade.php
+++ resources/views/partials/sidebar/headline.blade.php
@@ -1,6 +1,6 @@
 <li class="headline">
     <a href="#!">
-        <span class="mif-shop icon " ></span>
+        <span class="{{ config('app.icon') }} icon" ></span>
         <span class="title" >{{ config('app.name') }}</span>
 	</a>
 </li>
