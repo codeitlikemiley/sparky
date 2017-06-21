16e3a43bd18bba0c38a81997165b6a07fa9da0bd
Uriah Galang <g0ld3lux@gmail.com>
2017-06-21 18:35:19 +0800

    creataed logo.blade.php

---
+++ resources/views/partials/navbar/logo.blade.php
@@ -0,0 +1,3 @@
+<a href="#!" class="app-bar-element branding bg-hover-red"> 
+    <img src="{{ config('nav.brand.logo') }}" class="logo" style="height: 50px;width:175px;"> | {{ config('nav.brand.title') }}
+</a>
