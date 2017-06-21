03cc7fdcd8dcd44684a4a22d39716e49cd862276
Uriah Galang <g0ld3lux@gmail.com>
2017-06-21 18:34:59 +0800

    use partials.navbar.logo to reused this logo on other views

--- resources/views/partials/navbar.blade.php
+++ resources/views/partials/navbar.blade.php
@@ -1,10 +1,7 @@
 <div class="app-bar fixed-top bg-darker" data-role="appbar">
     <div class="container-top">
         <div class="sidebar-trigger sidebar-sizer mobile-only"><span class="fa fa-gear"></span></div>
-        <a href="#!" class="app-bar-element branding">
-             {!!config('nav.brand.logo')!!}
-             {!!config('nav.brand.title')!!}
-        </a>
+        @include('partials.navbar.logo')
         <ul class="app-bar-menu place-right">
             @if (Auth::guest())
             @include('partials.header.guest_link')
