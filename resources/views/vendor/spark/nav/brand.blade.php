6398c0414dc1da69019effcf1e9717cc2b117961
Uriah Galang <g0ld3lux@gmail.com>
2017-06-21 18:42:01 +0800

    change the default spark brand logo

--- resources/views/vendor/spark/nav/brand.blade.php
+++ resources/views/vendor/spark/nav/brand.blade.php
@@ -1,3 +1,6 @@
-<a class="navbar-brand" href="/home">
-    <img src="/img/mono-logo.png" style="height: 32px;">
+<!-- EDIT LOGO -->
+<a class="navbar-brand" href="{{ config('nav.home') }}">
+    <img src="{{ config('nav.brand.logo') }}" class="logo" style="height: 50px;widht:175px;">
+    <span>|</span>
+    {{ config('nav.brand.title') }}
 </a>
