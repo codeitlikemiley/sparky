32991dbe72fbb416728e4735efd54d59589738ea
Uriah Galang <g0ld3lux@gmail.com>
2017-06-21 13:18:38 +0800

    added nav config

---
+++ config/nav.php
@@ -0,0 +1,10 @@
+<?php
+
+return [
+    'brand' => [
+        'logo' => env('BRAND_LOGO', '<img src="http://logos-download.com/wp-content/uploads/2016/09/Laravel_logo_wordmark_logotype.png" class="logo" style="height: 50px;widht:175px;" >'),
+        'title' => env('BRAND_TITLE', 'Sparky.dev')
+    ],
+    'home' => '#!',
+
+];
