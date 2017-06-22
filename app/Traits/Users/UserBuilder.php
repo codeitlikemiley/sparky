5694296635b88eda3ba8f50b85c3b17501d82c9d
Uriah Galang <g0ld3lux@gmail.com>
2017-06-22 09:55:30 +0800

    added to UserBuilder To Autobuild Slug for username

--- app/Traits/Users/UserBuilder.php
+++ app/Traits/Users/UserBuilder.php
@@ -10,9 +10,11 @@ use App\Traits\Permissions\CanManageProjects;
 use App\Traits\Permissions\CanManageEmployees;
 use App\Traits\Mutators\UsersMutator;
 use App\Traits\StaticFunctions\UserStaticFunctions;
+use Cviebrock\EloquentSluggable\Sluggable;
+use App\Traits\Sluggable\UserSluggable;
 
 trait UserBuilder {
     use CanCreateClients, CanCreateEmployees, CanCreateProjects,
         CanManageClients, CanManageEmployees, CanManageProjects,
-        UsersMutator, UserStaticFunctions;
+        UsersMutator, UserStaticFunctions, Sluggable, UserSluggable;
 }
