3af5f08e19c86d5ef24e32868b638fa3f80de588
Uriah Galang <g0ld3lux@gmail.com>
2017-06-22 21:37:34 +0800

    added model factory for user , project, campaign, task, subtask

--- database/factories/ModelFactory.php
+++ database/factories/ModelFactory.php
@@ -18,7 +18,41 @@ $factory->define(App\User::class, function (Faker\Generator $faker) {
     return [
         'name' => $faker->name,
         'email' => $faker->unique()->safeEmail,
-        'password' => $password ?: $password = bcrypt('secret'),
+        'password' => $password ?: $password = 'secret',
         'remember_token' => str_random(10),
     ];
 });
+
+$factory->define(App\Project::class, function (Faker\Generator $faker) {
+    return [
+        'name' => $faker->name,
+    ];
+});
+
+$factory->define(App\Campaign::class, function (Faker\Generator $faker) {
+    return [
+        'name' => $faker->catchPhrase(),
+    ];
+});
+
+$factory->define(App\Task::class, function (Faker\Generator $faker) {
+    return [
+        'name' => $faker->catchPhrase(),
+        'description' => $faker->sentence($nbWords = 15, $variableNbWords = true),
+        'link' => $faker->url(),
+        'max_points' => $faker->numberBetween($min = 50, $max = 100),
+    ];
+});
+
+
+$factory->define(App\SubTask::class, function (Faker\Generator $faker) {
+
+    return [
+        'name' => $faker->catchPhrase(),
+        'assigned_points' => $faker->numberBetween($min = 1, $max = 10),
+        'priority' => $faker->numberBetween($min = 1, $max = 5),
+        'video_link' => $faker->url(),
+        'due_date' => \Carbon\Carbon::now()->addDays(10)->toDateTimeString(),
+    ];
+});
+
