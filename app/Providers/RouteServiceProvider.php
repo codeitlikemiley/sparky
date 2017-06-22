8619a6f20d3f26eb4804db26c36de6ea0191a7da
Uriah Galang <g0ld3lux@gmail.com>
2017-06-22 11:21:50 +0800

    added all route pattern, bind, model for our routes

--- app/Providers/RouteServiceProvider.php
+++ app/Providers/RouteServiceProvider.php
@@ -3,7 +3,15 @@
 namespace App\Providers;
 
 use Illuminate\Routing\Router;
+use Illuminate\Support\Facades\Route;
 use Illuminate\Foundation\Support\Providers\RouteServiceProvider as ServiceProvider;
+use App\User;
+use App\Employee;
+use App\Client;
+use App\Project;
+use App\Campaign;
+use App\Task;
+use App\Subtask;
 
 class RouteServiceProvider extends ServiceProvider
 {
@@ -23,9 +31,43 @@ class RouteServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        //
-
         parent::boot();
+        
+        Route::bind('username', function ($value) {
+        return User::where('username', $value)->first();
+        });
+        Route::pattern('username', '[a-z0-9_-]{3,16}');
+
+        Route::bind('projectSlug', function ($value) {
+        return User::where('slug', $value)->first();
+        });
+        Route::pattern('projectSlug', '[a-z0-9-]+');
+
+        Route::model('user', User::class);
+        Route::pattern('user', '[0-9]+');
+
+        Route::model('employee', Employee::class);
+        Route::pattern('employee', '[0-9]+');
+
+        Route::model('client', Client::class);
+        Route::pattern('client', '[0-9]+');
+
+        Route::model('projectID', Project::class);
+        Route::pattern('projectID', '[0-9]+');
+
+        Route::model('campaign', Campaign::class);
+        Route::pattern('campaign', '[0-9]+');
+
+        Route::model('task', Task::class);
+        Route::pattern('task', '[0-9]+');
+
+        Route::model('subtask', Subtask::class);
+        Route::pattern('subtask', '[0-9]+');
+
+        Route::pattern('id', '[0-9]+');
+        Route::pattern('nonwww', '[\/\w\.-]*');
+        Route::pattern('subdomain', '^(?!.*(admin|api|www)).*$');
+        
     }
 
     /**
