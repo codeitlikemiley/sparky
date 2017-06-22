54cadc84a10cca25cf2513da38899fdea3c8850e
Uriah Galang <g0ld3lux@gmail.com>
2017-06-22 10:52:28 +0800

    added mutators and static functions

--- app/Traits/Employees/EmployeeBuilder.php
+++ app/Traits/Employees/EmployeeBuilder.php
@@ -8,9 +8,12 @@ use App\Traits\Permissions\CanCreateEmployees;
 use App\Traits\Permissions\CanManageClients;
 use App\Traits\Permissions\CanManageProjects;
 use App\Traits\Permissions\CanManageEmployees;
+use App\Traits\Mutators\UsersMutator;
+use App\Traits\StaticFunctions\UserStaticFunctions;
 
 
 trait EmployeeBuilder {
     use CanCreateClients, CanCreateEmployees, CanCreateProjects,
-        CanManageClients, CanManageEmployees, CanManageProjects;
+        CanManageClients, CanManageEmployees, CanManageProjects,
+        UsersMutator, UserStaticFunctions;
 }
