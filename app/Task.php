e41ffd5237cfba1ee5dbc5099d60c14ec8e59b68
Uriah Galang <g0ld3lux@gmail.com>
2017-06-23 14:23:49 +0800

    added LogsActivity Trait and Override Methods

--- app/Task.php
+++ app/Task.php
@@ -4,10 +4,11 @@ namespace App;
 
 use Illuminate\Database\Eloquent\Model;
 use App\Traits\ModelBuilder\TaskBuilder;
+use Spatie\Activitylog\Traits\LogsActivity;
 
 class Task extends Model
 {
-    use TaskBuilder;
+    use TaskBuilder, LogsActivity;
 
     protected $table ='tasks';
 
@@ -26,6 +27,10 @@ class Task extends Model
 
     protected static $logAttributes = ['name', 'description', 'link', 'maxpoints'];
 
+    protected static $ignoreChangedAttributes = ['updated_at'];
+
+    protected static $logOnlyDirty = true;
+
     public function subtasks()
     {
         return $this->hasMany('App\SubTask','task_id', 'id');
@@ -34,5 +39,20 @@ class Task extends Model
     public function campaign()
     {
         return $this->belongsTo('App\Campaign', 'campaign_id', 'id');
+    }
+
+    public function getDescriptionForEvent(string $eventName): string
+    {
+        $guards = collect(config('auth.guards'))->keys()->all();
+        foreach ($guards as $guard) {
+           if(auth()->guard($guard)->check()){
+            $name = auth()->guard($guard)->user()->name;
+            break;
+           }
+        }
+        // No user
+        if(!$name)return "This model has been {$eventName}";
+        // With User
+        return "{$eventName} by: {$name}";
     }
 }
