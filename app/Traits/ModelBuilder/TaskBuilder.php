4053c1bb424cc1eeb835332330b3d6d6ea47801f
Uriah Galang <g0ld3lux@gmail.com>
2017-06-24 19:52:14 +0800

    Add Static Function to Get Progress on Task , Total points and Done Points

--- app/Traits/ModelBuilder/TaskBuilder.php
+++ app/Traits/ModelBuilder/TaskBuilder.php
@@ -4,7 +4,9 @@ namespace App\Traits\ModelBuilder;
 
 use BrianFaust\Commentable\HasComments;
 use App\Traits\Methods\LoggersMethod;
+use App\Traits\Methods\NormalizerMethod;
+use App\Traits\Methods\TaskProgressMethod;
 
 trait TaskBuilder {
-    use HasComments, LoggersMethod;
+    use HasComments, LoggersMethod, NormalizerMethod, TaskProgressMethod;
 }
