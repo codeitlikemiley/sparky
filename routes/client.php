a8baf3d7fa1f7e541737f1d5d27f642840f9def1
Uriah Galang <g0ld3lux@gmail.com>
2017-06-26 21:53:00 +0800

    added client route

--- routes/client.php
+++ routes/client.php
@@ -1,7 +1,17 @@
 <?php
-// URL format username.domain.com/client
-Route::get('/', function($username){
-    return 'Client Page with Tenants As : '.$username;
+Route::group(['domain' => '{username}.'.config('app.domain')], function () {
+  // Authentication Routes
+  Route::get('/login', 'Auth\LoginController@showLoginForm')->name('client.login');
+  Route::post('/login', 'Auth\LoginController@login')->name('client.login.submit');
+  Route::get('/logout', 'Auth\LoginController@logout')->name('client.logout');
+  // Password reset Routes
+  Route::post('/password/email', 'Auth\ForgotPasswordController@sendResetLinkEmail')->name('client.password.email');
+  Route::get('/password/reset', 'Auth\ForgotPasswordController@showLinkRequestForm')->name('client.password.request');
+  Route::post('/password/reset', 'Auth\ResetPasswordController@reset');
+  Route::get('/password/reset/{token}', 'Auth\ResetPasswordController@showResetForm')->name('client.password.reset');
+  // Admin Dashboard Route
+  Route::get('/', 'DashboardController@index')->name('admin.dashboard');
+  
 });
 
 
