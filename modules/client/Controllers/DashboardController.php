289677130318cb10391c3608dce8fe7288682cf0
Uriah Galang <g0ld3lux@gmail.com>
2017-06-26 21:52:07 +0800

    added modules for client and employee login

---
+++ modules/client/Controllers/DashboardController.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace Modules\Client\Controllers;
+
+use Illuminate\Http\Request;
+use Modules\Controller as BaseController;
+
+class DashboardController extends BaseController
+{
+    /**
+     * Create a new controller instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        $this->middleware('auth:client');
+    }
+
+    /**
+     * Show the application dashboard.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        return view('modules.client.dashboard');
+    }
+}
