753f4f7c5343071b0fba014d7609f8478f27d747
Uriah Galang <g0ld3lux@gmail.com>
2017-06-26 21:47:46 +0800

    change to Modules\Employee\Notifications\ResetPasswordNotification

--- app/Employee.php
+++ app/Employee.php
@@ -4,7 +4,7 @@ namespace App;
 
 use Illuminate\Notifications\Notifiable;
 use Illuminate\Foundation\Auth\User as Authenticatable;
-use Client\Notifications\ResetPasswordNotification;
+use Modules\Employee\Notifications\ResetPasswordNotification;
 use App\Traits\ModelBuilder\EmployeeBuilder;
 
 class Employee extends Authenticatable
