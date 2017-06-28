e9e0c27f8ad07fbe72981fe4297169e755dac4e3
Uriah Galang <g0ld3lux@gmail.com>
2017-06-28 13:59:55 +0800

    added routes to check type of auth

--- routes/api.php
+++ routes/api.php
@@ -14,5 +14,22 @@
 Route::group([
     'middleware' => 'auth:api'
 ], function () {
-    //
+        Route::get('/auth/user', function(){
+        if(auth()->check()){
+            return auth()->user();
+        }
+        return 'no auth user';
+    });
+    Route::get('/auth/employee', function(){
+        if(auth()->guard('employee')->check()){
+            return auth()->guard('employee')->user();
+        }
+        return 'no auth user';
+    });
+    Route::get('/auth/client', function(){
+        if(auth()->guard('client')->check()){
+            return auth()->guard('employee')->user();
+        }
+        return 'no auth user';
+    });
 });
