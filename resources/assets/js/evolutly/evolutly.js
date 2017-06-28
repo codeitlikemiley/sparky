30a1fda8cba2d06faa1103c0fcaa39315e85dd62
Uriah Galang <g0ld3lux@gmail.com>
2017-06-28 21:15:03 +0800

    added evolutluy.js file for webpack.mix.js

---
+++ resources/assets/js/evolutly/evolutly.js
@@ -0,0 +1,103 @@
+/**
+ * Export the root Spark application.
+ */
+module.exports = {
+    el: '#evolutly-app',
+
+
+    /**
+     * Initial FrontEnd State We Should Add this On The Backend
+     */
+    data: {
+        apiDomain: Evolutly.apiDomain,
+        domain: Evolutly.domain,
+        userId: Evolutly.userId,
+        employeeId: Evolutly.employeeId,
+        clientId: Evolutly.clientId,
+        user: Evolutly.state.user,
+    },
+
+
+    /**
+     * The component has been created by Vue.
+     */
+    created() {
+        var self = this;
+        
+        Bus.$on('updateUserData', function () {
+            self.getAuthUser();
+        });
+
+        Bus.$on('updateEmployeeData', function () {
+            self.getAuthEmployee();
+        });
+
+        Bus.$on('updateClientData', function () {
+            self.getAuthClient();
+        });
+
+    },
+
+
+    /**
+     * Prepare the application.
+     */
+    mounted() {
+        this.whenReady();
+    },
+
+
+    methods: {
+        /**
+         * Finish bootstrapping the application.
+         */
+        whenReady() {
+            //
+        },
+
+
+        /**
+         * Load the data for an authenticated user.
+         */
+        loadDataForAuthenticatedUser() {
+            this.getAuthUser();
+        },
+        loadDataForAuthenticatedEmployee() {
+            this.getAuthEmployee();
+        },
+        loadDataForAuthenticatedClient() {
+            this.getAuthClient();
+        },
+
+
+        /*
+         * Get the current authenticated user
+         */
+        getAuthUser() {
+            axios.get(this.apiDomain+'/auth/user')
+                .then(response => {
+                    this.user = response.data;
+                });
+        },
+
+        /*
+         * Get the current authenticated employee
+         */
+        getAuthEmployee() {
+            axios.get(this.apiDomain +'/auth/employee')
+                .then(response => {
+                    this.user = response.data;
+                });
+        },
+
+        /*
+         * Get the current authenticated Client
+         */
+        getAuthClient() {
+            axios.get(this.apiDomain +'/auth/client')
+                .then(response => {
+                    this.user = response.data;
+                });
+        },
+    },
+};
