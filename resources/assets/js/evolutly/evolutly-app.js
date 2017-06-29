4bd98e4fad405477cee60c4f0061ebec2729a724
Uriah Galang <g0ld3lux@gmail.com>
2017-06-29 20:19:22 +0800

    added projects

--- resources/assets/js/evolutly/evolutly-app.js
+++ resources/assets/js/evolutly/evolutly-app.js
@@ -15,6 +15,7 @@ module.exports = {
         employeeId: Evolutly.employeeId,
         clientId: Evolutly.clientId,
         user: Evolutly.state.user,
+        projects: Evolutly.state.user.projects
     },
 
 
