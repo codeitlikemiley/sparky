7113805975028fe91941550c9f0bec3c4c8c78cd
Uriah Galang <g0ld3lux@gmail.com>
2017-06-29 20:20:39 +0800

    added projects main view in dashboard

--- resources/views/modules/employee/dashboard.blade.php
+++ resources/views/modules/employee/dashboard.blade.php
@@ -1,19 +1,55 @@
 @extends('evolutly::layouts.app') 
+
+@push('critical_css')
+<style>
+    .demo-grid .row > .cell {
+    background-color: #002933;
+}
+</style>
+@endpush
+
 @section('content')
-<dashboard :user="user" inline-template>
+<dashboard :user="user" :projects="projects" inline-template>
+<div> 
+    <div class="section-wrapper page-heading ">
+        <h3>Welcome @{{ user.name }}</h3>
+    </div>
+    <div class="section-wrapper bg-white align-center">
+        <h1>Overall Progress</h1>
+    </div>
 
-        <!-- Application Dashboard -->
-        <div class="row">
-            <div class="">
-                <div class="panel panel-default">
-                    <div class="panel-heading">Dashboard</div>
-
-                    <div class="panel-body">
-                        Your application's dashboard.
+<div class="section-wrapper animated fadeInRightBig">
+        <div class="grid condensed demo-grid">
+                <div class="row cells3" v-for="item in projectChunks">
+                    <div class="cell" v-for="project in item">
+                        <div class="panel error">
+                            <div class="heading">
+                                <span class="title">@{{ project.name }}</span>
+                            </div>
+                            <div class="content">
+                                <div class="tile-container row cells2">
+                                    <div class="tile bg-amber fg-white cell" data-role="tile">
+                                        <div class="tile-content iconic cell">
+                                            <span class="icon fa fa-flag"></span>
+                                            <span class="tile-badge bg-lightOrange" style="margin-right: -14px; margin-bottom: 2px;">5</span>
+                                            <span class="tile-label">Campaigns</span>
+                                        </div>
+                                    </div>
+                                    
+                                    <div class="tile bg-teal fg-white cell element-selected" data-role="tile">
+                                        <div class="tile-content iconic cell">
+                                            <span class="icon fa fa-tasks"></span>
+                                            <div class="tile-label">Progress</div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
                     </div>
                 </div>
-            </div>
-        </div>
+    </div> 
+</div>
 
+</div>
 </dashboard>
 @endsection
