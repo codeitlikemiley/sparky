aa36962c5434fd65c6fca10fa2b487e2f8405b2a
Uriah Galang <g0ld3lux@gmail.com>
2017-07-02 19:50:57 +0800

    added CampaignProgress and ShowProject Controller

---
+++ modules/employee/Controllers/Project/ShowProject.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace Modules\Employee\Controllers\Project;
+
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller as BaseController;
+
+class ShowProject extends BaseController
+{
+    /**
+     * Create a new controller instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        $this->middleware('auth:employee');
+    }
+
+    /**
+     * Receive Project Id
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function __invoke($tenant,$project)
+    {
+        return view('project::view',['project' => $project, 'tenant' => $tenant]);
+    }
+}
