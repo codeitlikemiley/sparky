adacc4bc27717b07b892c34006e6ce1532bfdb41
Uriah Galang <g0ld3lux@gmail.com>
2017-07-02 19:49:09 +0800

    added sticky footer

---
+++ resources/views/modules/evolutly/partials/footer.blade.php
@@ -0,0 +1,7 @@
+<div class="footer bg-darker">
+    <footer class="align-center">
+        <p style="margin-top:20px;" class="fg-amber">
+            <strong>Evolutly.com ©</strong> Director's Cut, LLC 2017
+        <p>
+    </footer>
+</div>
