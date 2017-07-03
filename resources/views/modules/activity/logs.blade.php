f504d803724f2d0eeaf548ace740e6e4c7f4453f
Uriah Galang <g0ld3lux@gmail.com>
2017-07-03 19:05:33 +0800

    added activity view

---
+++ resources/views/modules/activity/logs.blade.php
@@ -0,0 +1,63 @@
+<div class="cell">
+    <div class="panel widget-box warning" data-role="panel">
+        <div class="heading align-center">
+            <div class="title">Activity Logs</div>
+        </div>
+        <div class="content">
+            
+            <ul class="timeline-list">
+                <li>
+                    <div class="date">
+                        <span class="icon fa fa-briefcase"></span>
+                        <div>6:00 am</div>
+                        <small>2 hrs ago</small>
+                    </div>
+                    <div class="content">
+                        <h5>Updated By: User 1</h5>
+                        <div>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Minima laboriosam amet quibusdam provident
+                            deserunt.</div>
+                    </div>
+                </li>
+
+                <li>
+                    <div class="date">
+                        <span class="icon fa fa-envelope"></span>
+                        <div>7:00 am</div>
+                        <small>1 hrs ago</small>
+                    </div>
+                    <div class="content">
+                        <h5>Updated By: User 2</h5>
+                        <div>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Minima laboriosam amet quibusdam provident
+                            deserunt.</div>
+                    </div>
+                </li>
+
+                <li>
+                    <div class="date">
+                        <span class="icon fa fa-users"></span>
+                        <div>10:00 am</div>
+                        <small>1 hrs ago</small>
+                    </div>
+                    <div class="content">
+                        <h5>Created By: User 1</h5>
+                        <div>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Minima laboriosam amet quibusdam provident
+                            deserunt.</div>
+                    </div>
+                </li>
+            </ul>
+            <div class="align-center">
+                <span class="tag info">
+                    <span class="icon mif-arrow-down"></span> load more...
+                </span>
+                <span class="tag info">
+                    <span class="icon mif-blocked"></span> No More Activity To Load
+                </span>
+                <span class="tag info">
+                    <span class="icon mif-more-horiz"></span>
+                </span>
+            </div>
+
+
+        </div>
+    </div>
+</div>
