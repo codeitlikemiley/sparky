37c38f50690de8b4740a574956f660110bd26030
Uriah Galang <g0ld3lux@gmail.com>
2017-07-03 19:06:23 +0800

    adde subtask view

---
+++ resources/views/modules/subtask/table.blade.php
@@ -0,0 +1,98 @@
+<div class="row cells">
+    <div class="cell">
+        <div class="panel widget-box " data-role="panel">
+            <div class="heading">
+                <div class="title">Subtasks</div>
+            </div>
+            <div class="content">
+                <div class="sub-heading bg-chess">
+                    <a href="javascript:void(0);" onclick="alert('Please Create A Component to Add Subtask');">
+                    <button class="button info"><span class="mif-plus"></span> Add Subtask</button>
+                    </a>
+                </div>
+                <div class="text">
+
+                    <div class="table-responsive ">
+
+                        <table class="table border bordered striped ">
+                            <thead class="">
+                                <tr>
+                                    <th>Subtask</th>
+                                    <th>Points</th>
+                                    <th>Priority</th>
+                                    <th>Video</th>
+                                    <th>Status</th>
+                                    <th>Due Date</th>
+                                    <th>Assigned To</th>
+                                    <th>Action</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                <tr>
+                                    <td>Create Wordpress Site</td>
+                                    <td class="align-center">5</td>
+                                    <td class="align-center">
+                                        <span class="icon mif-star-full fg-amber">
+                                        </span>
+                                        <span class="icon mif-star-full fg-amber">
+                                        </span>
+                                        <span class="icon mif-star-full fg-amber">
+                                        </span>
+                                        <span class="icon mif-star-full fg-amber">
+                                        </span>
+                                        <span class="icon mif-star-full fg-amber">
+                                        </span>
+                                    </td>
+                                    <td class="align-center">
+                                        <span class="tag info">
+                                            <span class="icon mif-play">
+                                            </span>
+                                        </span>
+                                    </td>
+                                    <td class="align-center">
+                                        <span class="tag bg-amber fg-white">Ongoing
+                                        </span>
+                                    </td>
+                                    <td>
+                                    May 31, 2017
+                                    </td>
+                                    <td>
+                                        <span class="tag bg-amber fg-white">user1
+                                        </span>
+                                        <span class="tag bg-amber fg-white">user2
+                                        </span>
+                                        <span class="tag bg-amber fg-white">user3
+                                        </span>
+                                    </td>
+                                    <td class="align-center" style="font-size: 1.25rem">
+                                        <span class="fg-lightBlue icon mif-pencil">
+                                        </span>
+                                        <span class="fg-red icon fa fa-trash">
+                                        </span>
+
+                                        <span class="tag success">
+                                            <span class="icon mif-checkmark">
+                                                 
+                                            </span>
+                                        </span>
+                                        <span class="fg-amber icon mif-blocked">
+                                        </span>
+                                    </td>
+                                </tr>
+                                
+                            </tbody>
+                        </table>
+
+                    </div>
+
+
+
+                </div>
+            </div>
+        </div>
+
+
+
+
+    </div>
+</div>
