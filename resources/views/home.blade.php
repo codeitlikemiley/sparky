93b6c45c40305151042fa21c3199b16179f3e003
Uriah Galang <g0ld3lux@gmail.com>
2017-07-04 14:45:19 +0800

    change to the default dashboard for projects

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,20 +1,50 @@
-@extends('layouts.main')
-
+@extends('evolutly::layouts.app') 
+@push('critical_css') 
+@include('css::grid') 
+@include('css::footer') 
+@endpush 
 @section('content')
-<home :user="userId" inline-template>
-    <div class="container">
-        <!-- Application Dashboard -->
-        <div class="row">
-            <div class="col-md-8 col-md-offset-2">
-                <div class="panel panel-default">
-                    <div class="panel-heading">Dashboard</div>
+<dashboard :user="user" :projects="projects" inline-template>
+    <div>
+        <div class="section-wrapper page-heading ">
+            <h3>Welcome @{{ user.name }}</h3>
+        </div>
+        <div class="section-wrapper bg-white align-center">
+            <h1>Projects</h1>
+        </div>
 
-                    <div class="panel-body">
-                        Your application's dashboard.
+        <div class="section-wrapper animated fadeInRightBig">
+            <div class="grid condensed demo-grid">
+                <div class="row cells3" v-for="item in projectChunks">
+                    <div class="cell" v-for="project in item">
+                        <div class="panel error">
+                            <div class="heading">
+                                <span class="title">@{{ project.name }}</span>
+                            </div>
+                            <div class="content">
+                                <div class="tile-container row cells2">
+                                    <div class="tile bg-amber fg-white cell" data-role="tile" @click="viewProject(project.id)">
+                                        <div class="tile-content iconic cell">
+                                            <span class="icon fa fa-flag"></span>
+                                            <span class="tile-badge bg-lightOrange" style="margin-right: -14px; margin-bottom: 2px;">5</span>
+                                            <span class="tile-label">Campaigns</span>
+                                        </div>
+                                    </div>
+
+                                    <div class="tile bg-teal fg-white cell element-selected" data-role="tile" @click="viewProgress(project.id)">
+                                        <div class="tile-content iconic cell">
+                                            <span class="icon fa fa-tasks"></span>
+                                            <div class="tile-label">Progress</div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+
+                        </div>
                     </div>
                 </div>
             </div>
         </div>
     </div>
-</home>
+</dashboard>
 @endsection
