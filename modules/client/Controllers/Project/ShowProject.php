0f0d82b5d0cd55a686a455d3a7ff50c723ab9f96
Uriah Galang <g0ld3lux@gmail.com>
2017-07-04 14:51:00 +0800

    added Client Controllers

---
+++ modules/client/Controllers/Project/ShowProject.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace Modules\Client\Controllers\Project;
+
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller as BaseController;
+
+class ShowProject extends BaseController
+{
+    /**
+     * Create a new controller instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        $this->middleware('auth:client');
+    }
+
+    /**
+     * Receive Project Id
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function __invoke($tenant,$project)
+    {
+        return view('project::view',['project' => $project, 'tenant' => $tenant]);
+    }
+}
