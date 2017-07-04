0a077679a95bf2634fad0cd241abb4ee44f8a0db
Uriah Galang <g0ld3lux@gmail.com>
2017-07-04 14:48:38 +0800

    check if tenant is set (if your using subdomain) if not then user the curent user name

--- resources/views/modules/evolutly/partials/sidebar/headline.blade.php
+++ resources/views/modules/evolutly/partials/sidebar/headline.blade.php
@@ -1,6 +1,12 @@
 <li class="headline">
     <a>
         <span class="{{ config('app.icon') }} icon" ></span>
-        <small>{{ $tenant->name }}</small>
+        <small>
+            @if(isset($tenant))
+            {{ $tenant->name }}
+            @else
+           {{ auth()->user()->name }}
+            @endif
+            </small>
 	</a>
 </li>
