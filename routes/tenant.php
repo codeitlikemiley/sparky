fc7130c51c450e8c99b03c894dbb6e4b2046145f
Uriah Galang <g0ld3lux@gmail.com>
2017-07-04 14:52:56 +0800

    added tenant routes for subdomain

---
+++ routes/tenant.php
@@ -0,0 +1,10 @@
+<?php
+Route::group(['domain' => '{username}.'.config('app.domain')], function () {
+Route::group(['prefix' => '/dashboard'], function () {
+      Route::get('/', 'DashboardController@index')->name('dashboard');
+      // Route::get('/projects/create', 'Project\CreateProject')->name('employee.projects.create');
+      Route::get('/projects/{projectID}', 'Project\ShowProject')->name('tenant.projects.view');
+      Route::get('/projects/{projectID}/progress', 'Project\CampaignsProgress')->name('tenant.projects.progress');
+      Route::get('/tasks/{task}', 'Task\ShowTask')->name('tenant.tasks.view');
+  });
+});
