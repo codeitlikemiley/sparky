65fdcf15a496434b61c01145ce11a1884131f30e
Uriah Galang <g0ld3lux@gmail.com>
2017-07-06 05:32:48 +0800

    remove /home and replace with /dashboard

--- resources/views/partials/navbar/front_user.blade.php
+++ resources/views/partials/navbar/front_user.blade.php
@@ -16,7 +16,7 @@
                     width: 50px;">
                     {{ auth()->user()->name }}</a>
                 <ul class="d-menu context place-right" data-role="dropdown">
-                    <li><a href="{{url('/home')}}"><span class="mif-windows icon" ></span> Dashboard</a></li>
+                    <li><a href="{{url('/dashboard')}}"><span class="mif-windows icon" ></span> Dashboard</a></li>
                     <li><a href="{{url('/')}}"><span class="mif-home icon" ></span>Front End</a></li>
                     <li class="divider"></li>
                     <li class="">
