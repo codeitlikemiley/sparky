b7fc1659a111f425dd22c2b19a2535db590bf3c1
Uriah Galang <g0ld3lux@gmail.com>
2017-07-05 20:12:25 +0800

    added image intervention service provider and facade'

--- config/app.php
+++ config/app.php
@@ -176,6 +176,7 @@ return [
         BrianFaust\Commentable\CommentableServiceProvider::class,
         Spatie\Activitylog\ActivitylogServiceProvider::class,
         Collective\Html\HtmlServiceProvider::class,
+        Intervention\Image\ImageServiceProvider::class,
         
         /*
          * Application Service Providers...
@@ -239,6 +240,7 @@ return [
         'View' => Illuminate\Support\Facades\View::class,
         'Form' => Collective\Html\FormFacade::class,
         'Html' => Collective\Html\HtmlFacade::class,
+        'Image' => Intervention\Image\Facades\Image::class,
 
     ],
 
