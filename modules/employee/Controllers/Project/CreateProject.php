3f4631f72210884c5a6cf89540e1ff3aabe7d01a
Uriah Galang <g0ld3lux@gmail.com>
2017-07-07 15:30:35 +0800

    added controller for creating project for employee and tenant, note tenant controller is being over written by the web

---
+++ modules/employee/Controllers/Project/CreateProject.php
@@ -0,0 +1,73 @@
+<?php
+
+namespace Modules\Employee\Controllers\Project;
+
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller as BaseController;
+use Illuminate\Database\Eloquent\ModelNotFoundException;
+use App\Project;
+use App\Client;
+
+class CreateProject extends BaseController
+{
+    
+    protected $project;
+
+    protected $input;
+
+    protected $client;
+
+    public function __construct(Project $project,Request $request, Client $client)
+    {
+        $this->middleware('auth:employee');
+        $this->input = $request->all();
+        $this->project = $project;
+        $this->client = $client;
+    }
+
+    /**
+     * Receive Project Id
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function __invoke($tenant)
+    {
+        $this->createProject($tenant);
+        return redirect()->back();
+    }
+
+    private function hasClient($tenant)
+    {
+        if(isset($this->input['client_id'])){
+            return $this->validateClient($tenant);
+        }
+    }
+
+    private function createProject($tenant)
+    {
+        $user = auth()->guard('employee')->user();
+        $project = $this->project;
+        $project->name = $this->input['project_name'];
+        $project->tenant_id = $tenant->id;
+
+        if($this->hasClient($tenant)){
+            $client = $this->getClient();
+            $client->projects()->save($project);
+        }
+        $user->projects()->save($project);
+    }
+
+    private function validateClient($tenant)
+    {
+        $client =  $this->getClient();
+        if($client->tenant_id === $tenant->id){
+            return true;
+        }
+        return false;
+    }
+
+    private function getClient()
+    {
+        return $this->client->findOrFail($this->input['client_id']);
+    }
+}
