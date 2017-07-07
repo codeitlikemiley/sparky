d291bbb19218f7cc0dca557041e7e8596c980475
Uriah Galang <g0ld3lux@gmail.com>
2017-07-07 15:34:47 +0800

    added route for tenant not yet working though, this is for wildcard subdomain project creation

--- routes/tenant.php
+++ routes/tenant.php
@@ -1,8 +1,9 @@
 <?php
 Route::group(['domain' => '{username}.'.config('app.domain')], function () {
 Route::group(['prefix' => '/dashboard'], function () {
-      Route::get('/', 'DashboardController@index')->name('dashboard');
-      // Route::get('/projects/create', 'Project\CreateProject')->name('employee.projects.create');
+      // fix this tenant.dashboard being over written by the web route
+      Route::get('/', 'DashboardController@index')->name('tenant.dashboard');
+      Route::post('/projects/create', 'Project\CreateProject')->name('tenant.projects.create');
       Route::get('/projects/{projectID}', 'Project\ShowProject')->name('tenant.projects.view');
       Route::get('/projects/{projectID}/progress', 'Project\CampaignsProgress')->name('tenant.projects.progress');
       Route::get('/tasks/{task}', 'Task\ShowTask')->name('tenant.tasks.view');
