0ab7daf638524b0660ab416e52c7eaffec4466e7
Uriah Galang <g0ld3lux@gmail.com>
2017-07-08 13:30:21 +0800

    eager load project

--- modules/employee/Controllers/Project/ShowProject.php
+++ modules/employee/Controllers/Project/ShowProject.php
@@ -4,6 +4,7 @@ namespace Modules\Employee\Controllers\Project;
 
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller as BaseController;
+use App\Project;
 
 class ShowProject extends BaseController
 {
@@ -24,6 +25,9 @@ class ShowProject extends BaseController
      */
     public function __invoke($tenant,$project)
     {
+        // We need to Get the Current Workers
+        // We need to Get All Files related to this Project
+        $project->load('campaigns.tasks','assignedEmployees')->toArray();
         return view('project::view',['project' => $project, 'tenant' => $tenant]);
     }
 }
