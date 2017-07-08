0b319e9209458d043e9d6b5d5dbf5fb71ee62841
Uriah Galang <g0ld3lux@gmail.com>
2017-07-08 13:29:06 +0800

    dynamically loaded data for state, by default return user and tenant, other depends on the routes being hit

--- modules/evolutly/InitialFrontendState.php
+++ modules/evolutly/InitialFrontendState.php
@@ -5,6 +5,11 @@ namespace Modules\Evolutly;
 use Modules\Evolutly\Contracts\InitialFrontendState as Contract;
 use Illuminate\Auth\AuthManager;
 use App\Project;
+use Route;
+use App\Task;
+use App\Employee;
+use App\User;
+use App\Client;
 
 class InitialFrontendState implements Contract
 {
@@ -16,11 +21,59 @@ class InitialFrontendState implements Contract
     }
     public function forUser($user)
     {
-        return [
-            'user' => $this->currentUser(),
-            'projects' => $this->projects()
-        ];
+        // make a method to determine what variable for user we will return
+        return $this->getData();
     }
+
+    protected function getData() {
+        // we always return a user and a tenant
+        $data = array_merge(array(),['user' => $this->currentUser()]);
+        $data = array_merge($data,['tenant' => $this->getTenant()]);
+        // if we are on the dashboard of the all types of user
+        $dashboardRoutes = array("dashboard", "tenant.dashboard", "employee.dashboard", "client.dashboard");
+        if (in_array(Route::currentRouteName(), $dashboardRoutes)) {
+        $data = array_merge($data,['projects' => $this->projects()]);
+        }
+        // if we are on the specific project page
+        // This Part is Working But WE Will Comment This Out For the Mean Time
+        // $projectRoutes = array("employee.projects.view", "client.projects.view", "tenant.projects.view");
+        // if (in_array(Route::currentRouteName(), $projectRoutes)) {
+        // $data = array_merge($data,['currentProject' => $this->currentProject()]);
+        // $data = array_merge($data,['currentProjectFiles' => $this->currentProjectFiles()]);
+        // $data = array_merge($data,['currentProjectWorkers' => $this->currentProjectWorkers()]);
+        // }
+        return $data;
+        
+    }
+    protected function getTenant()
+    {
+        $user = $this->currentUser();
+        if($user instanceOf User){
+            return $user; 
+        }
+        return User::find($user->tenant_id)->select('id','username','name','photo_url')->first();
+    }
+    // Maybe we can do a eager loading here....
+    protected function currentProject()
+    {
+        // invoke here a controller to get url segment for example {id}
+        // then fetch the currentProject
+        return Project::find(1);
+    }
+    protected function currentProjectFiles()
+    {
+        // $this->currentProject()
+        return Task::all();
+    }
+    protected function currentProjectWorkers()
+    {
+        // $this->currentProject()
+        return Employee::all();
+    }
+
+    // default should only be the user
+    // for dashboard we add projects
+    // fro project we add project , campaigns, files, workers
 
     protected function projects()
     {
