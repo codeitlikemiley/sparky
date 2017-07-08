b5a7091e7e7176e5f541489e45739423bfc94751
Uriah Galang <g0ld3lux@gmail.com>
2017-07-08 22:51:45 +0800

    make main vue instance slim

--- resources/assets/js/evolutly/evolutly-app.js
+++ resources/assets/js/evolutly/evolutly-app.js
@@ -6,39 +6,26 @@ module.exports = {
 
 
     /**
-     * Initial FrontEnd State We Should Add this On The Backend
+     * Initial FrontEnd State
      */
 
     data: {
+        // loaded by default
         user: Evolutly.state.user,
         tenant: Evolutly.state.tenant,
+        // loaded depending on route
         projects: (Evolutly.state.projects ? Evolutly.state.projects : []),
+        // for review if we will remove this or just use json_encode
         'current_project': (Evolutly.state.currentProject ? Evolutly.state.currentProject : null),
         'project_files': (Evolutly.state.currentProjectFiles ? Evolutly.state.currentProjectFiles : []),
         'project_workers': (Evolutly.state.currentProjectWorkers ? Evolutly.state.currentProjectWorkers : [])
     },
 
-
     /**
      * The component has been created by Vue.
      */
     created() {
-        var self = this;
-        
-        Bus.$on('updateUserData', function () {
-            self.getAuthUser();
-        });
-
-        Bus.$on('updateEmployeeData', function () {
-            self.getAuthEmployee();
-        });
-
-        Bus.$on('updateClientData', function () {
-            self.getAuthClient();
-        });
-
     },
-
 
     /**
      * Prepare the application.
@@ -47,58 +34,9 @@ module.exports = {
         this.whenReady();
     },
 
-
     methods: {
-        /**
-         * Finish bootstrapping the application.
-         */
         whenReady() {
             //
-        },
-
-
-        /**
-         * Load the data for an authenticated user.
-         */
-        loadDataForAuthenticatedUser() {
-            this.getAuthUser();
-        },
-        loadDataForAuthenticatedEmployee() {
-            this.getAuthEmployee();
-        },
-        loadDataForAuthenticatedClient() {
-            this.getAuthClient();
-        },
-
-
-        /*
-         * Get the current authenticated user
-         */
-        getAuthUser() {
-            axios.get(this.apiDomain+'/auth/user')
-                .then(response => {
-                    this.user = response.data;
-                });
-        },
-
-        /*
-         * Get the current authenticated employee
-         */
-        getAuthEmployee() {
-            axios.get(this.apiDomain +'/auth/employee')
-                .then(response => {
-                    this.user = response.data;
-                });
-        },
-
-        /*
-         * Get the current authenticated Client
-         */
-        getAuthClient() {
-            axios.get(this.apiDomain +'/auth/client')
-                .then(response => {
-                    this.user = response.data;
-                });
         },
     },
 };
