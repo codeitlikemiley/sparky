c65ebcfa1620a06f55740a935d5d1c8431bcb0b7
Uriah Galang <g0ld3lux@gmail.com>
2017-07-14 19:37:44 +0800

    extract to smaller views

--- resources/views/modules/project/content.blade.php
+++ resources/views/modules/project/content.blade.php
@@ -1,16 +1,6 @@
 <div class="content">
     <div class="text">
-        <div>
-            <a href="#!">
-                <button @click="show('add-campaign')" class="button info"><span class="mif-plus"></span> Add New Campaign</button>
-            </a>
-            <a href="#!">
-                <button @click="show('edit-project')" class="button info"><span class="mif-pencil"></span> Edit Project</button>
-            </a>
-            <a href="#!">
-                <button @click="deleteProject()" class="button alert"><span class="icon fa fa-trash"></span> Delete Project</button>
-            </a>
-        </div>
+        @include('project::action_buttons')
         <div class="row cells12">
             <div class="cell colspan12">
                 <div class="tabcontrol" data-role="tabcontrol">
@@ -21,7 +11,10 @@
                         <li><a href="#people_tab">People</a></li>
                     </ul>
                     <div class="frames bg-white">
-                        @include('task::tab') @include('form::tab') @include('file::tab') @include('employee::tab')
+                        @include('task::tab')
+                        @include('form::tab')
+                        @include('file::tab')
+                        @include('employee::tab')
                     </div>
                 </div>
             </div>
