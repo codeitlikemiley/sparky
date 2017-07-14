b228062b497bf7edaa221a5b367fbb01ce06c392
Uriah Galang <g0ld3lux@gmail.com>
2017-07-15 00:01:51 +0800

    extract css to truncate

--- resources/views/modules/task/tab.blade.php
+++ resources/views/modules/task/tab.blade.php
@@ -1,25 +1,15 @@
-@push('critical_css')
-<style>
-.truncate {
-  width: 50%;
-  white-space: nowrap;
-  overflow: hidden;
-  text-overflow: ellipsis;
-}
-</style>
-@endpush
 <div class="frame bg-white margin-bottom-90" id="tasks_tab">
     <div class="panel widget-box collapsible" data-role="panel">
         <div class="row cells2" v-for="(chunk, keyChunk) in campaignChunks(campaigns)"
             :key="keyChunk">
 
-            <draggable class="cell colspan1 draghandle" v-for="(campaign, cKey) in chunk" :options="{group: 'campaign',handle: '.draghandle',filter: '.v--modal-box,.v--modal',scroll: true,preventOnFilter: false}"
+            <draggable class="cell colspan1" v-for="(campaign, cKey) in chunk" :options="{group: 'campaign',handle: '.draghandle',filter: '.v--modal-box,.v--modal',scroll: true,preventOnFilter: false}"
                 :data-id="campaign.id" :key="cKey" @end="onEnd" style="min-height: 150px;" :leftOrRight="cKey" :max="keyChunk"
                 :data-id="campaign.id">
                 <div class="panel">
 
-                    <div class="heading  bg-lightBlue" style="cursor:move;">
-                        <div class="truncate" style="cursor:pointer;position:absolute;top:12px;margin-left:136px;">
+                    <div class="heading draghandle bg-lightBlue" style="cursor:move;">
+                        <div style="position:absolute;top:12px;margin-left:136px;">
                             <span data-role="hint" data-hint-mode="2" data-hint="Switch|Re-Order Campaign" data-hint-position="top">
                                 @{{ campaign.name }}
                             </span>
