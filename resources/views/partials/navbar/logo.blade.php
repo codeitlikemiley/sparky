5ad72060362be689d67978f6340ea9472a826bbd
Uriah Galang <g0ld3lux@gmail.com>
2017-08-04 17:48:43 +0800

    test logo

--- resources/views/partials/navbar/logo.blade.php
+++ resources/views/partials/navbar/logo.blade.php
@@ -1,6 +1,6 @@
 <a href="#!" class="app-bar-element branding bg-hover-red"> 
     @isset(request()->username->username)
-    <img src="{{ request()->username->username . config('app.domain').'/img/logo.png' }}" class="logo" style="height: 50px;width:175px;">
+    <img src="{{ config('app.url').'/img/logo.png' }}" class="logo" style="height: 50px;width:175px;">
     @endisset
     @empty(request()->username->username)
     <img src="{{ config('app.url').'/img/logo.png' }}" class="logo" style="height: 50px;width:175px;">
--- resources/views/vendor/spark/nav/brand.blade.php
+++ resources/views/vendor/spark/nav/brand.blade.php
@@ -1,7 +1,7 @@
 <!-- EDIT LOGO -->
 <a class="navbar-brand" href="{{ config('nav.home') }}">
         @isset(request()->username->username)
-        <img src="{{ request()->username->username . config('app.domain').'/img/logo.png' }}" class="logo" style="height: 50px;width:175px;">
+        <img src="{{ config('app.url').'/img/logo.png' }}" class="logo" style="height: 50px;width:175px;">
         @endisset
         @empty(request()->username->username)
         <img src="{{ config('app.url').'/img/logo.png' }}" class="logo" style="height: 50px;width:175px;">
