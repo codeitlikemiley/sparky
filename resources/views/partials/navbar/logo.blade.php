e8841a9b3bf50c6d78634b6460163b19656df521
Uriah Galang <g0ld3lux@gmail.com>
2017-08-04 17:39:00 +0800

    fix logo

--- resources/views/partials/navbar/logo.blade.php
+++ resources/views/partials/navbar/logo.blade.php
@@ -1,3 +1,8 @@
 <a href="#!" class="app-bar-element branding bg-hover-red"> 
+    @isset(request()->username->username)
+    <img src="{{ Request::username()->username . config('app.domain').'/img/logo.png' }}" class="logo" style="height: 50px;width:175px;"> | {{ config('nav.brand.title') }}
+    @endisset
+    @empty(request()->username->username)
     <img src="{{ config('app.url').'/img/logo.png' }}" class="logo" style="height: 50px;width:175px;"> | {{ config('nav.brand.title') }}
+    @endempty
 </a>
