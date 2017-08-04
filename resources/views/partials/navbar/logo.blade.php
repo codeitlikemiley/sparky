f38e2e7a2f563b44b9401c30ecf6f80202514643
Uriah Galang <g0ld3lux@gmail.com>
2017-08-04 12:56:43 +0800

    use app url

--- resources/views/partials/navbar/logo.blade.php
+++ resources/views/partials/navbar/logo.blade.php
@@ -1,3 +1,3 @@
 <a href="#!" class="app-bar-element branding bg-hover-red"> 
-    <img src="{{ config('nav.brand.logo') }}" class="logo" style="height: 50px;width:175px;"> | {{ config('nav.brand.title') }}
+    <img src="{{ config('app.url').'/img/logo.png' }}" class="logo" style="height: 50px;width:175px;"> | {{ config('nav.brand.title') }}
 </a>
