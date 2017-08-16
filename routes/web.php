490a09df1194cd3d2af3847f6f120ec7112e5778
Uriah Galang <g0ld3lux@gmail.com>
2017-08-17 04:05:55 +0800

    remove unnessary route

--- routes/web.php
+++ routes/web.php
@@ -54,9 +54,6 @@ Route::group(['prefix' => '/dashboard'], function () {
       Route::post('/clients', 'User\AddClient')->name('tenant.clients.add');
       Route::put('/clients/{client}/edit', 'User\EditClient')->name('tenant.clients.edit');
       Route::delete('/clients/{client}/delete', 'User\DeleteClient')->name('tenant.clients.delete');
-    //   Route::put('/clients/{client}/attach', 'User\AssignProject')->name('tenant.clients.assign_projects');
-    //   Route::delete('/clients/{client}/projects/detach/{projectID}', 'User\UnassignedProject')->name('tenant.clients.detach_projects');
-    //   ROute::put('/clients/{client}/projects/sync', 'User\SyncProjects')->name('tenant.clients.sync_projects');
   });
 Route::get('/files', 'File\Index')->name('tenant.files.index');
 Route::post('/files/upload/{projectID}', 'File\UploadController@multiple_upload')->name('tenant.file.uploader');
