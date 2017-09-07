37c75109d4b09d79d1443dd1737ee88a476f6f8f
Uriah Galang <g0ld3lux@gmail.com>
2017-09-07 18:07:22 +0800

    delete all orphaned relationship connected to users

--- app/Http/Controllers/User/DeleteTenant.php
+++ app/Http/Controllers/User/DeleteTenant.php
@@ -4,10 +4,8 @@ namespace App\Http\Controllers\User;
 
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller as BaseController;
-use App\User;
-use App\Notifications\RegistrationWelcomeEmail;
-use Carbon\Carbon;
-use Laravel\Spark\Spark;
+use Laravel\Spark\Notification;
+use DB;
 
 class DeleteTenant extends BaseController
 {
@@ -32,12 +30,20 @@ class DeleteTenant extends BaseController
      */
     public function __invoke($tenant)
     {
+        if($tenant->isSuperAdmin()){
+            $this->message = 'Opps Cant Delete Super Admin';
+            $this->code = 403;
+            return response()->json(['message' => $this->message], $this->code);
+        }
         if($this->getAuth()->id === $this->getTenant()->id){
             
-            if($tenant->projects){
-                $tenant->projects()->delete();
-            }
-            // add db constraint on delete Cascade to automatically delete
+            $this->deleteProjects($tenant);
+            $this->deleteEmployees($tenant);
+            $this->deleteClients($tenant);
+            $this->deleteNotifications($tenant);
+            $this->deleteAnnouncements($tenant);
+            $this->deleteFiles($tenant);
+            // $this->deleteComments($tenant); // client // employee
             $tenant->delete();
             return response()->json(['message' => $this->message, 'user' => $tenant], $this->code);
         }
@@ -45,4 +51,65 @@ class DeleteTenant extends BaseController
         $this->message = 'UnAuthorized Action';
         return response()->json(['message' => $this->message], $this->code);
     }
+
+    private function deleteProjects($tenant){
+        if($tenant->projects){
+            $tenant->projects()->each(function ($project) {
+                if($project->campaigns){
+                    $project->campaigns()->each(function($campaign){
+                        if($campaign->tasks){
+                            $campaign->tasks()->each(function($task){
+                                if($task->subtasks){
+                                    $task->subtasks()->each(function($subtask){
+                                        $subtask->employees()->sync([]);
+                                        $subtask->delete();
+                                    });
+                                }
+                                
+                                $task->delete();
+                            });
+                        }
+                        
+                        $campaign->delete();
+                    });
+                }
+                
+                $project->delete();
+             });
+        }
+    }
+
+    private function deleteEmployees($tenant){
+        if($tenant->employees){
+            $tenant->employees()->each(function ($employee) {
+                $employee->delete();
+             });
+        }
+    }
+
+    private function deleteClients($tenant){
+        if($tenant->clients){
+            $tenant->clients()->each(function ($client) {
+                $client->delete();
+             });    
+        }
+    }
+
+    private function deleteNotifications($tenant){
+        DB::table('notifications')->where('user_id',$tenant->id)->delete();
+        
+    }
+
+    private function deleteAnnouncements($tenant){
+        DB::table('announcements')->where('user_id',$tenant->id)->delete();
+    }
+
+    private function deleteFiles($user){
+        if($user->files){
+            $user->files()->each(function ($file) {
+                $file->delete();
+             });
+        }
+    }
+
 }
